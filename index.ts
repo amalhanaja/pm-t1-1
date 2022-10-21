2ba83c9eede59d3b208aa3a16cfdfabae53b68b8
amalhanaja <amalhanaja@gmail.com>
2022-10-21 20:44:04 +0700

    reformat

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 const showText = (trafficLightColor: string): void => {
-    const colorLowerCase = trafficLightColor.toLowerCase();
-    let textToShow: string = "";
-    switch (colorLowerCase){
-        case "merah":
-            textToShow = "Berhenti";
-            break;
-        case "kuning": 
-            textToShow = "Hati-hati";
-            break;
-        case "hijau":
-            textToShow = "Jalan"
-            break;
-        default:
-            textToShow = "Unknown color"
-            break;
-    }
-    console.log(textToShow);
-}
-
+  const colorLowerCase = trafficLightColor.toLowerCase();
+  let textToShow: string = "";
+  switch (colorLowerCase) {
+    case "merah":
+      textToShow = "Berhenti";
+      break;
+    case "kuning":
+      textToShow = "Hati-hati";
+      break;
+    case "hijau":
+      textToShow = "Jalan";
+      break;
+    default:
+      textToShow = "Unknown color";
+      break;
+  }
+  console.log(textToShow);
+};
